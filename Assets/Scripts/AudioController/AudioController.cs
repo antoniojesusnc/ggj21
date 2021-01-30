d5f74d57a03034cca2065e3a737df55a63b90624
Antonio <antoniojesusnc@gmail.com>
2021-01-30 21:08:07 +0100

    audio contoller

---
+++ Assets/Scripts/AudioController/AudioController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController Instance;
+    
+    [SerializeField]
+    private AudioData _audioData;
+
+    private List<AudioSource> _audioSource;
+
+    void Start()
+    {
+        Instance = this;
+        _audioSource = new List<AudioSource>();
+    }
+
+    public void StopAllSounds()
+    {
+        for (int j = 0; j < _audioSource.Count; j++)
+        {
+            _audioSource[j].Stop();
+        }
+    }
+    
+    public void StopSound(EAudioType audioType)
+    {
+        for (int i = 0; i < _audioData.audios.Count; i++)
+        {
+            if (_audioData.audios[i].type == audioType &&
+                _audioData.audios[i].audios.Count > 0)
+            {
+                StopSoundInternal(_audioData.audios[i]);
+                break;
+            }
+        }
+    }
+
+    private void StopSoundInternal(AudioDataInfo audioDataAudio)
+    {
+        for (int i = 0; i < audioDataAudio.audios.Count; i++)
+        {
+            for (int j = 0; j < _audioSource.Count; j++)
+            {
+                if (audioDataAudio.audios[i] = _audioSource[j].clip)
+                {
+                    _audioSource[j].Stop();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void PlaySound(EAudioType audioType)
+    {
+        for (int i = 0; i < _audioData.audios.Count; i++)
+        {
+            if (_audioData.audios[i].type == audioType &&
+                _audioData.audios[i].audios.Count > 0)
+            {
+                PlaySoundInternal(_audioData.audios[i]);
+            }
+        }
+    }
+
+    private void PlaySoundInternal(AudioDataInfo audioInfo)
+    {
+        var randomClipIndex = Random.Range(0, audioInfo.audios.Count);
+        var audioClip = audioInfo.audios[randomClipIndex];
+        
+        var audioSource = GetAudioSource();
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        audioSource.loop = audioInfo.isLoop;
+    }
+
+    private AudioSource GetAudioSource()
+    {
+        AudioSource audioSource = null;
+        for (int i = 0; i < _audioSource.Count; i++)
+        {
+            if (!_audioSource[i].isPlaying)
+            {
+                audioSource = _audioSource[i];
+                break;
+            }
+        }
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        return audioSource;
+    }
+}
---
+++ Assets/Scripts/AudioController/Data/AudioData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioData", menuName = "Data/New Audio Data", order = 1)]
+public class AudioData : ScriptableObject
+{
+    public List<AudioDataInfo> audios;
+}
+
+[System.Serializable]
+public class AudioDataInfo
+{
+    public EAudioType type;
+    public List<AudioClip> audios;
+    public bool isLoop;
+}
+
+public enum EAudioType
+{
+    MainMusic,
+    BarLvl1,
+    BarLvl2,
+    BarLvl3,
+    GameOver,
+    Victory,
+    ThiefStep,
+    GuardStep,
+    SFXWall,
+    SFXSteal
+    
+}
+
