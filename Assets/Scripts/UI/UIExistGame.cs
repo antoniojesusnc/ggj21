065143b050e9345041e0833831eb9a38cd89a218
Antonio <antoniojesusnc@gmail.com>
2021-01-31 14:52:48 +0100

    exit game and adjust to grid

---
+++ Assets/Scripts/UI/UIExistGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIExistGame : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex <= 0)
+            {
+                Application.Quit();
+            }else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }        
+    }
+}
